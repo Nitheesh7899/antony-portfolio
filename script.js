76342d238c087d487eeaef594ee16f89fc80277c
Nitheesh7899 <114846619+Nitheesh7899@users.noreply.github.com>
2023-09-06 15:02:41 +0530

    commiting

---
+++ script.js
@@ -0,0 +1,9 @@
+function About(){
+    window.open("http://127.0.0.1:5500/About/About.html");
+}
+function Experience(){
+    window.open("http://127.0.0.1:5500/Experience/experience.html");
+}
+function Contact(){
+    window.open("http://127.0.0.1:5500/Contact/Contact.html");
+}
